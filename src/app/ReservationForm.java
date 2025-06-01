8e98df004ca3ea9388b17f148edea5b20eb39bac
{leonhard316325@gmail.com} <abe.risa@chiba-u.jp>
2025-06-01 12:19:16 +0900

    make reservation forms & ReservationViewer

--- src/app/ReservationForm.java
+++ src/app/ReservationForm.java
@@ -1,5 +1,9 @@
 package app;
+import java.util.Properties;
+import java.util.ArrayList;
 import javax.swing.*;
+import java.util.Vector;
+
 
 public class ReservationForm {
     public static void main(String[] args){
@@ -15,7 +19,45 @@ public class ReservationForm {
         // コンポーネント追加
         panel.add(new JLabel("名前"));
         panel.add(new JTextField(20));
-        panel.add(new JComboBox<>(new String[]{"M","F"}));
+        // panel.add(new JComboBox<>(new String[]{"M","F"}));
+        JRadioButton maleButton = new JRadioButton("男性");
+        JRadioButton femaleButton = new JRadioButton("女性");
+        // 性別ラジオボタンをグループ化
+        ButtonGroup genderGroup = new ButtonGroup();
+        genderGroup.add(maleButton);
+        genderGroup.add(femaleButton);
+        panel.add(maleButton);
+        panel.add(femaleButton);
+        // 診療科目の選択
+
+        // 問診入力
+        JTextField syusoField = new JTextField(25);
+        panel.add(syusoField);
+        // 日付選択
+        ArrayList<String> years = new ArrayList<>();
+        ArrayList<String> months = new ArrayList<>();
+        ArrayList<String> days = new ArrayList<>();
+        for (int i = 2025; i < 2030; i++){
+            years.add(String.valueOf(i));
+        }
+        for (int i = 1; i < 12; i++){
+            months.add(String.valueOf(i));
+        }
+        for (int i = 1; i < 32; i++){
+            days.add(String.valueOf(i));
+        }
+        JComboBox<String> yearBox = new JComboBox<>(new Vector<>(years));
+        JComboBox<String> monthBox = new JComboBox<>(new Vector<>(months));
+        JComboBox<String> dayBox = new JComboBox<>(new Vector<>(days));
+
+        panel.add(new JLabel("予約日:"));
+        panel.add(yearBox);
+        panel.add(new JLabel("年"));
+        panel.add(monthBox);
+        panel.add(new JLabel("月"));
+        panel.add(dayBox);
+        panel.add(new JLabel("日"));
+
         frame.setVisible(true);
     }
 }
